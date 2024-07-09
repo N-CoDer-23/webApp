2a80b919bf4970c986725fde23f82e9f0e437d0a
Nuriddin2004 <ortiqboevnuriddin1@gmail.com>
2024-07-09 06:50:59 +0500

    yangscsd

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,63 @@
 import React, { useState } from 'react';
 import { useTelegram } from '../hooks/TelegramWebApp';
-import './Main.css'
+import './Main.css';
+import hamsterImage from '../assets/lion.jpg'; // Hamster image URL
 
 const Main = () => {
     const { onClose, increment, decrement } = useTelegram();
     const [count, setCount] = useState(0);
 
     return (
-        <div className='MainBox'>
-            <div className="admincontroller">
-                 <h1>Count: {count}</h1>
-            <div className="buttonContainer">
-                <button onClick={() => decrement(count, setCount)}>Minus</button>
-                <button onClick={() => increment(count, setCount)}>Plus</button>
-                <button onClick={onClose}>Orqaga</button>
+        <div className="main-box">
+            <div className="admin-controller">
+                <div className="header"> 
+                    <div className="profit">
+                        <h1>Lion Combat</h1>
+                    
+                    
+                    </div>
+                    <div className="profile">
+                    <img src={hamsterImage} alt="" className='profile-pic'/>
+                        <div>
+                            <h2>Nuriddin (CEO)</h2>
+                            <div className="level">
+                                <span>Grandmaster</span>
+                                <span>9 / 11</span>
+                            </div>
+                        </div>
+                        <button className='x' onClick={onClose}>X</button>
+                    </div>
+                   
+                </div>
+                <div className="task-container">
+                    <div className="task">
+                        <span>Vazifalar ro'yxati</span>
+                        <span>18:59</span>
+                    </div>
+                    <div className="task">
+                        <span>Kundalik shifr</span>
+                        <span>13:59</span>
+                    </div>
+                    <div className="task">
+                        <span>Kundalik kombinatsiya</span>
+                        <span>06:59</span>
+                    </div>
+                </div>
+                <div className="currency">
+                    <span>{count}</span>
+                </div>
+                <div className="hamster-image">
+                {/* <button >Plus</button> */}
+                    <img src={hamsterImage} alt="Hamster" onClick={() => increment(count, setCount)}/>
+                </div>
+                <div className="button-container">
+                    <button onClick={() => decrement(count, setCount)}>Minus</button>
+                    
+                    
+                </div>
             </div>
-            </div>
-           
         </div>
-    )
-}
+    );
+};
 
-export default Main
+export default Main;
